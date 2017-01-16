68202d689d0e1af5d110701042df187abbbf791b
John Szwaronek <johnsz9999@gmail.com>
2017-01-16 14:33:38 -0500

    Initial copy from feathers-an-introduction

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// import errors from 'feathers-errors';
-import makeDebug from 'debug';
-
-const debug = makeDebug('feathers-guide');
-
-export default function init () {
-  debug('Initializing feathers-guide plugin');
-  return 'feathers-guide';
-}
